c9f29c64ae96302b683b081660375ef2e54020a2
nohjjhlove@hanmail.net <nohjjh@naver.com>
2025-06-30 22:10:40 +0900

    add와 commit 동시에

--- test_folder/basic_syntax/1.basic_command.sh
+++ test_folder/basic_syntax/1.basic_command.sh
@@ -1,3 +1,21 @@
 
 # git add . 은 모든변경(추가)사항을 staging area 이동
 git add .
+# git commit은 staging area의 변경사항을 확정짓고 commitID생성
+git commit -m "메시지"
+
+# 원격 repo에 main브랜치 변경사항(commitID)을 업로드
+git push origin main
+
+# 현재 working directory, staging area의 상태 조회
+git status 
+
+# 특정 파일만 add할 경ㅇ
+git add 특정파일(위치포함)
+
+# local repository에 커밋이력 생성
+git commit -m "메시지타이틀" -m "메시지내용"
+
+#vi에디터 모드에서 작성 : 첫줄에는 제목, 두번째줄부터 내용
+git commit
+
